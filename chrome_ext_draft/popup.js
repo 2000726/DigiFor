709f608dfbdf722de8364a32200e8fe3506269fd
Shahmizan <71887108+2000726@users.noreply.github.com>
2021-10-28 23:01:39 +0800

    Update popup.js

--- chrome_ext_draft/popup.js
+++ chrome_ext_draft/popup.js
@@ -1,1 +1,16 @@
 
+window.addEventListener('load', function() { 
+  var range = document.getElementById('timeRange')
+  var cache = false
+  
+  var downloadCSV = document.getElementById('downloadCSV')
+  downloadCSV.onclick = function(element) {
+    if (cache) {
+      downloadCSV(cache)
+      return
+    }
+  }
+}
+    
+    
+  
